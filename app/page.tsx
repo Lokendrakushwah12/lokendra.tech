57e105b0fa1b7ba0c6bc8b1836cb53e71c72829a
Lokendra <lokendrakushwah8051@gmail.com>
2026-02-08 21:12:47 +0530

    Added a ProjectsFallback component

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,14 @@ import Header from "@/components/sections/header";
 import Projects from "@/components/sections/projects";
 import LinesBG from "@/components/ui/lines-bg";
 import { ProgressiveBlur } from "@/components/ui/progressive-blur";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="flex flex-col h-full lg:max-w-4xl overflow-clip mx-auto relative">
       <ClientDither />
       <ProgressiveBlur
-        className="pointer-events-none z-[500] fixed bottom-0 w-full h-20"
+        className="pointer-events-none z-500 fixed bottom-0 w-full h-20"
         direction="bottom"
         blurIntensity={1}
       />
@@ -22,9 +23,23 @@ export default function Page() {
       <LinesBG />
       <Experience />
       <LinesBG />
-      <Projects />
+      <Suspense fallback={<ProjectsFallback />}>
+        <Projects />
+      </Suspense>
       <LinesBG />
       <Footer />
+    </div>
+  );
+}
+
+function ProjectsFallback() {
+  return (
+    <div className="border-b border-border border-dashed">
+      <div className="border-x border-border border-dashed p-4 max-w-screen-xl w-full mx-auto space-y-4 py-8 md:py-16">
+        <div className="w-full inline-flex items-center justify-center gap-1 font-normal tracking-tight text-xl text-muted-foreground">
+          Projects
+        </div>
+      </div>
     </div>
   );
 }
